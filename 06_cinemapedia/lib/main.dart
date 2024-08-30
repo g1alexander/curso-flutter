65a84f99574b6c3a61a96bbe8bd5ab8d19daea45
g1alexander <alexander.granados.dev@gmail.com>
2024-08-29 21:40:10 -0500

    part 1 cinemapedia

---
+++ 06_cinemapedia/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      routerConfig: appRouter,
+    );
+  }
+}
