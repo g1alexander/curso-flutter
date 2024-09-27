77f1be019f6b9867e91e26c41979a2b0b7db5630
g1alexander <me@g1alexander.com>
2024-09-26 22:01:59 -0500

    run in ios

--- 08_push_app/lib/main.dart
+++ 08_push_app/lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   await NotificationsBloc.initializeFCM();
-  await LocalNotifications.initialize();
+  // await LocalNotifications.initialize();
 
   runApp(MultiBlocProvider(providers: [
     BlocProvider(
